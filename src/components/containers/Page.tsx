18da20d42ae0f4343d4054ee5fc6ab0237be9bef
Bell <ds@inbox.ru>
2024-06-09 14:46:15 +0300

    Restore ok files

--- src/components/containers/Page.tsx
+++ src/components/containers/Page.tsx
@@ -1,46 +1,28 @@
-import { CSSProperties, ReactElement, ReactNode } from 'react';
+import { CSSProperties, ReactNode } from 'react';
 import './Page.styles.css';
 import Container from './Container';
-import MainButton from '../buttons/MainButton';
+import { usePageState } from '../../hooks/usePage';
+import Title from '../typography/Title';
 
 type Props = {
   children: ReactNode;
-  title?: ReactElement;
   style?: CSSProperties;
   className?: string;
-<<<<<<< HEAD
-  onAction?: () => void;
-  actionTitle?: string;
 };
 
-function Page({
-  children,
-  style,
-  className,
-  title,
-  actionTitle,
-  onAction,
-}: Props) {
-  return (
-    <>
-      <Container style={style} className={className}>
-        {title}
-        {children}
-      </Container>
-      <MainButton title={actionTitle} onClick={onAction} />
-    </>
-=======
-  description?: ReactNode;
-};
-
-function Page({ children, style, className, title, description }: Props) {
+function Page({ children, style, className }: Props) {
+  const { title } = usePageState();
   return (
     <Container style={style} className={className}>
-      {title}
-      {description}
+      {title.title && (
+        <Title
+          title={title.title}
+          titleAccent={title.titleAccent}
+          hintMessage={title.hintMessage}
+        />
+      )}
       {children}
     </Container>
->>>>>>> remotes/origin/start-page-changes-finish
   );
 }
 
