97cae4481c139326e99738179fb90e1ca3b45e50
Vardan <vardanter@yandex.ru>
2024-06-27 01:22:48 +0400

    change brackets

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,23 +1,23 @@
-import { createBrowserRouter } from 'react-router-dom';
-import PlayGround from './pages/playGround';
-// import AddWallet from './components/start-page/AddWallet/AddWallet.tsx';
-// import YourSecretKey from './components/start-page/AddWallet/create-wallet/your-secret-key/YourSecretKey.tsx';
+import { createBrowserRouter } from "react-router-dom";
+import PlayGround from "./pages/playGround";
+// import AddWallet from "./components/start-page/AddWallet/AddWallet.tsx";
+// import YourSecretKey from "./components/start-page/AddWallet/create-wallet/your-secret-key/YourSecretKey.tsx";
 
-import Welcome from './pages/registration/Welcome.tsx';
-import ConfirmKey from './pages/registration/ConfirmKey.tsx';
-import AddWallet from './pages/registration/AddWallet.tsx';
-import SecretKey from './pages/registration/SecretKey.tsx';
-import RegistrationIsCompleted from './pages/registration/RegistrationIsCompleted.tsx';
-import Existing from './pages/registration/Existing.tsx';
-import Main from './pages/Main.tsx';
-import GamePage from './pages/playGround/gamePage';
-import LeaderBoard from './pages/playGround/leaderBoard';
-import CategoryGames from './pages/playGround/categoryGames';
+import Welcome from "./pages/registration/Welcome.tsx";
+import ConfirmKey from "./pages/registration/ConfirmKey.tsx";
+import AddWallet from "./pages/registration/AddWallet.tsx";
+import SecretKey from "./pages/registration/SecretKey.tsx";
+import RegistrationIsCompleted from "./pages/registration/RegistrationIsCompleted.tsx";
+import Existing from "./pages/registration/Existing.tsx";
+import Main from "./pages/Main.tsx";
+import GamePage from "./pages/playGround/gamePage";
+import LeaderBoard from "./pages/playGround/leaderBoard";
+import CategoryGames from "./pages/playGround/categoryGames";
 import PinCode from "./pages/pincode/PinCode.tsx";
-import AddCrypto from './pages/addCrypto';
-import ReceiveAsset from './pages/addCrypto/ReceiveAsset.tsx';
-import ChooseAddMethod from './pages/addCrypto/AddCrypto.tsx';
-import AddCryptoAddress from './pages/addCrypto/Address.tsx';
+import AddCrypto from "./pages/addCrypto";
+import ReceiveAsset from "./pages/addCrypto/ReceiveAsset.tsx";
+import ChooseAddMethod from "./pages/addCrypto/AddCrypto.tsx";
+import AddCryptoAddress from "./pages/addCrypto/Address.tsx";
 import Account from "./pages/Account.tsx";
 
 const router = createBrowserRouter(
