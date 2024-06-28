7499c36b868dfac4156b41e3b9812b9404b949d2
Bell <ds@inbox.ru>
2024-06-28 19:23:12 +0300

    wallet api

--- src/types/wallet.ts
+++ src/types/wallet.ts
@@ -1,5 +1,13 @@
+import { Address } from '@ton/core';
+
 export interface AuthInitTon {
   address: string;
   publicKey: string;
   network: string;
+}
+
+export interface WalletBalanceData {
+  address: Address;
+  usdPrice: number;
+  changePrice: number;
 }
