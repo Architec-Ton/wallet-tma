1adb526fc9668070099086a50a73a628fa914ad2
Bell <ds@inbox.ru>
2024-07-04 05:47:02 +0300

    Games APi

--- src/components/ui/games/list/index.tsx
+++ src/components/ui/games/list/index.tsx
@@ -1,4 +1,3 @@
-
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { SwiperSlide } from 'swiper/react';
@@ -8,38 +7,42 @@ import Row from '../../../containers/Row';
 import Slider from '../../slider';
 import GameListItemGroup from '../listItemGroup';
 
-import './index.css'
+import './index.css';
 import useLanguage from '../../../../hooks/useLanguage';
 
 type OwnPropsType = {
-  games: GameListType<GameListItemType[]>
-}
-
+  games: GameListType<GameListItemType[]>;
+};
 
 function slideMatrix(arr: GameListItemType[], size: number) {
-  var res = [] 
-  for(let i = 0; i < arr.length; i = i + size) {
-    res.push(arr.slice(i, i + size))
+  const res = [];
+  for (let i = 0; i < arr.length; i = i + size) {
+    res.push(arr.slice(i, i + size));
   }
-  return res
+  return res;
 }
 
 const GameList = ({ games }: OwnPropsType) => {
-  const t = useLanguage("game")
-  const [gameList, setGameList] = useState<GameListType<GameListItemType[][]>>([])
+  const t = useLanguage('game');
+  const [gameList, setGameList] = useState<GameListType<GameListItemType[][]>>(
+    []
+  );
 
   useEffect(() => {
-    const list = games.reduce((acc, game): GameListType<GameListItemType[][]> => {
-      const {title, id, items} = game
-      acc.push({
-        title,
-        id,
-        items: slideMatrix(items as GameListItemType[], 3)
-      })
-      return acc
-    }, [] as GameListType<GameListItemType[][]>)
-    setGameList(list)
-  }, [])
+    const list = games.reduce(
+      (acc, game): GameListType<GameListItemType[][]> => {
+        const { title, id, apps } = game;
+        acc.push({
+          title,
+          id,
+          apps: slideMatrix(apps as GameListItemType[], 5),
+        });
+        return acc;
+      },
+      [] as GameListType<GameListItemType[][]>
+    );
+    setGameList(list);
+  }, []);
 
   return (
     <Column className="w-screen">
@@ -47,28 +50,36 @@ const GameList = ({ games }: OwnPropsType) => {
         <Column key={category.id}>
           <Row className="category-header">
             <div className="category-header__title">{category.title}</div>
-            <Link to={`/playground/category/${category.id}`} className="category-header__all">{t("all")}</Link>
+            <Link
+              to={`/playground/category/${category.id}`}
+              className="category-header__all">
+              {t('all')}
+            </Link>
           </Row>
           <Column>
-            <Slider settings={{
-              slidesPerView: "auto",
-              centeredSlides: true,
-              centeredSlidesBounds: true,
-              spaceBetween: 0
-            }}  className="list-swipe">
-              {category.items.map((itemsGroup, index) => {
+            <Slider
+              settings={{
+                slidesPerView: 'auto',
+                centeredSlides: true,
+                centeredSlidesBounds: true,
+                spaceBetween: 0,
+              }}
+              className="list-swipe">
+              {category.apps.map((itemsGroup, index) => {
                 return (
-                  <SwiperSlide key={`game_slide-${category.id}-${index}`} className="list-slide">
+                  <SwiperSlide
+                    key={`game_slide-${category.id}-${index}`}
+                    className="list-slide">
                     <GameListItemGroup group={itemsGroup} />
                   </SwiperSlide>
-                )
+                );
               })}
             </Slider>
           </Column>
         </Column>
       ))}
     </Column>
-  )
-}
+  );
+};
 
-export default GameList
+export default GameList;
--- src/features/gaming/gamingApi.ts
+++ src/features/gaming/gamingApi.ts
@@ -1,16 +1,16 @@
-import { createApi } from "@reduxjs/toolkit/query/react";
+import { createApi } from '@reduxjs/toolkit/query/react';
 import {
   GameCategoryType,
   GameListItemType,
   GameListType,
   IGame,
   TGameLeader,
-} from "../../types/gameTypes";
-import { setCategories } from "./gamingSlice";
-import baseQuery from "../api/api";
+} from '../../types/gameTypes';
+import { setCategories } from './gamingSlice';
+import baseQuery from '../api/api';
 
 export const gamingApi = createApi({
-  reducerPath: "gamingApi",
+  reducerPath: 'gamingApi',
   baseQuery: baseQuery,
   endpoints: (builder) => ({
     getCategories: builder.mutation<
@@ -18,8 +18,8 @@ export const gamingApi = createApi({
       string | undefined
     >({
       query: (search) => ({
-        url: !!search ? `games?search=${search}` : "games",
-        method: "GET",
+        url: !!search ? `apps?search=${search}` : 'apps',
+        method: 'GET',
       }),
       async onCacheEntryAdded(_, { dispatch, cacheDataLoaded }) {
         const cacheData = await cacheDataLoaded;
@@ -36,7 +36,7 @@ export const gamingApi = createApi({
       { id: string; limit?: number }
     >({
       query: ({ id, limit = 0 }) =>
-        `leaders/?gameId=${id}${limit ? `&_limit=${limit}` : ""}`,
+        `leaders/?gameId=${id}${limit ? `&_limit=${limit}` : ''}`,
     }),
     getCategoryGames: builder.query<
       GameCategoryType<GameListItemType[]>,
@@ -50,14 +50,14 @@ export const gamingApi = createApi({
     >({
       query: ({ id, params }) => ({
         url: `games/${id}?${params.toString()}`,
-        method: "GET",
+        method: 'GET',
       }),
     }),
     getTopRateGames: builder.mutation<
       GameListItemType[],
       URLSearchParams | undefined
     >({
-      query: (params) => (!params ? "top" : `top?${params.toString()}`),
+      query: (params) => (!params ? 'top' : `top?${params.toString()}`),
     }),
   }),
 });
