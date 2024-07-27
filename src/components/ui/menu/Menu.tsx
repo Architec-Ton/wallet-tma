0c2dfb7c29ca9cb3f65b9eb5f47754af0f9a19e3
Iurii Bellavin <iurii.bellavin@gmail.com>
2024-07-27 03:52:07 +0300

    change menu

--- src/components/ui/menu/Menu.tsx
+++ src/components/ui/menu/Menu.tsx
@@ -1,9 +1,9 @@
-import classNames from 'classnames';
-import { CSSProperties } from 'react';
-import { NavLink, useLocation } from 'react-router-dom';
-import './Menu.styles.css';
-import { usePage } from '../../../hooks/usePage';
-import { useClosure } from '../../../hooks/useClosure';
+import classNames from "classnames";
+import { CSSProperties } from "react";
+import { NavLink, useLocation } from "react-router-dom";
+import "./Menu.styles.css";
+import { useClosure } from "../../../hooks/useClosure";
+import useRouter from "../../../hooks/useRouter";
 
 export interface MenuItem {
   to: string;
@@ -18,27 +18,30 @@ interface MenuProps {
 }
 
 function Menu({ menuItems, style, className }: MenuProps) {
-  const page = usePage();
-  const location = useLocation()
+  // const page = usePage();
+  const location = useLocation();
+  const navigate = useRouter();
 
   const handlerClick = useClosure((to: string) => {
-    if (location.pathname !== to)
-      page.setLoading(true, false);
+    if (location.pathname !== to) {
+      //page.setLoading(true, false);
+      navigate(to);
+    }
   });
 
   return (
-      <nav className={classNames(className, 'menu-nav')} style={style}>
-        <div className="menu">
-          {menuItems.map((item) => (
-              <NavLink to={item.to} key={item.to} onClick={handlerClick(item.to)}>
-                {item.icon && (
-                    <img src={item.icon} alt={item.label} aria-label={item.label} />
-                )}
-                <p>{item.label}</p>
-              </NavLink>
-          ))}
-        </div>
-      </nav>
+    <nav className={classNames(className, "menu-nav")} style={style}>
+      <div className="menu">
+        {menuItems.map((item) => (
+          <NavLink to={item.to} key={item.to} onClick={handlerClick(item.to)}>
+            {item.icon && (
+              <img src={item.icon} alt={item.label} aria-label={item.label} />
+            )}
+            <p>{item.label}</p>
+          </NavLink>
+        ))}
+      </div>
+    </nav>
   );
 }
 
