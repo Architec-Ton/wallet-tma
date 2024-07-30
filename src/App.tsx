45d95e5cc727a6156d8843ce8d54c078321b9d34
Iurii Bellavin <iurii.bellavin@gmail.com>
2024-07-30 20:02:04 +0300

    Add ga

--- src/App.tsx
+++ src/App.tsx
@@ -13,9 +13,9 @@ import AlertContainer from "./components/ui/alert/AlertContainer.tsx";
 
 // import { Buffer } from "buffer/";
 // window.Buffer = Buffer;
-import ReactGA from 'react-ga4';
+import ReactGA from "react-ga4";
 
-ReactGA.initialize('G-R0B900V0LM');
+ReactGA.initialize("G-G09LBL852");
 
 function App() {
   return (
