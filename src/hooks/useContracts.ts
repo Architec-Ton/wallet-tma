fbf7dd117ca1466c28f5763344879ffafed481f0
egorkqq <egorkqq@gmail.com>
2024-08-08 13:01:25 +0300

    chore: rework envs

--- src/hooks/useContracts.ts
+++ src/hooks/useContracts.ts
@@ -1,96 +1,74 @@
-import { useTonClient } from "./useTonClient";
-import { Address, beginCell, OpenedContract, toNano } from "@ton/core";
-import { BankJettonWallet, Stake } from "../contracts/tact_BankJettonWallet";
-import { useTon } from "./useTon/index";
-import { BankJetton } from "../contracts/tact_BankJetton";
-import {
-  BANK_CROWDSALE_ADDRESS,
-  BANK_GAS_AMOUNT,
-  BANK_JETTON_MASTER_ADDRESS,
-} from "../constants";
-import { StakeStorage } from "../contracts/tact_StakeStorage";
-import {
-  BanksCrowdSaleV3 as BanksCrowdSale,
-  ReferralAddress,
-} from "../contracts/tact_BanksCrowdSaleV3";
-import { ArcJetton } from "../contracts/tact_ArcJetton";
-import {
-  ArcJettonWallet,
-  JettonTransfer,
-} from "../contracts/tact_ArcJettonWallet";
-import { useMemo } from "react";
+import { useTonClient } from './useTonClient'
+import { Address, beginCell, OpenedContract, toNano } from '@ton/core'
+import { BankJettonWallet, Stake } from '../contracts/tact_BankJettonWallet'
+import { useTon } from './useTon/index'
+import { BankJetton } from '../contracts/tact_BankJetton'
+import { BANK_CROWDSALE_ADDRESS, BANK_GAS_AMOUNT, BANK_JETTON_MASTER_ADDRESS } from '../constants'
+import { StakeStorage } from '../contracts/tact_StakeStorage'
+import { BanksCrowdSaleV3 as BanksCrowdSale, ReferralAddress } from '../contracts/tact_BanksCrowdSaleV3'
+import { ArcJetton } from '../contracts/tact_ArcJetton'
+import { ArcJettonWallet, JettonTransfer } from '../contracts/tact_ArcJettonWallet'
+import { useMemo } from 'react'
 
 function useContracts() {
-  const { client } = useTonClient();
-  const { sender } = useTon();
-
-  // if (network === CHAIN.MAINNET) {
-  //   return null;
-  // }
+  const { client } = useTonClient()
+  const { sender } = useTon()
 
   const jettonWallet = useMemo(() => {
     return (address: Address): OpenedContract<ArcJettonWallet> | undefined => {
-      if (!client) return;
-      const contract = ArcJettonWallet.fromAddress(address);
-      return client.open(contract) as OpenedContract<ArcJettonWallet>;
-    };
-  }, [client]);
+      if (!client) return
+      const contract = ArcJettonWallet.fromAddress(address)
+      return client.open(contract) as OpenedContract<ArcJettonWallet>
+    }
+  }, [client])
   const jettonMaster = useMemo(() => {
     return (address: Address): OpenedContract<ArcJetton> | undefined => {
-      if (!client) return;
-      const contract = ArcJetton.fromAddress(address);
-      return client.open(contract) as OpenedContract<ArcJetton>;
-    };
-  }, [client]);
+      if (!client) return
+      const contract = ArcJetton.fromAddress(address)
+      return client.open(contract) as OpenedContract<ArcJetton>
+    }
+  }, [client])
 
   const bankJettonWallet = useMemo(() => {
     return (address: Address): OpenedContract<BankJettonWallet> | undefined => {
-      if (!client) return;
-      const contract = BankJettonWallet.fromAddress(address);
-      return client.open(contract) as OpenedContract<BankJettonWallet>;
-    };
-  }, [client]);
+      if (!client) return
+      const contract = BankJettonWallet.fromAddress(address)
+      return client.open(contract) as OpenedContract<BankJettonWallet>
+    }
+  }, [client])
   const bankJettonMaster = useMemo(() => {
     return (): OpenedContract<BankJetton> | undefined => {
       if (!client) {
-        console.log("bankJettonMaster null", client);
-        return;
+        console.log('bankJettonMaster null', client)
+        return
       }
-      const contract = BankJetton.fromAddress(
-        Address.parse(BANK_JETTON_MASTER_ADDRESS)
-      );
-      return client.open(contract) as OpenedContract<BankJetton>;
-    };
-  }, [client]);
+      const contract = BankJetton.fromAddress(Address.parse(BANK_JETTON_MASTER_ADDRESS))
+      return client.open(contract) as OpenedContract<BankJetton>
+    }
+  }, [client])
 
   const bankCrowdSale = useMemo(() => {
     return (): OpenedContract<BanksCrowdSale> | undefined => {
-      if (!client) return;
-      const contract = BanksCrowdSale.fromAddress(
-        Address.parse(BANK_CROWDSALE_ADDRESS)
-      );
-      return client.open(contract) as OpenedContract<BanksCrowdSale>;
-    };
-  }, [client]);
+      if (!client) return
+      const contract = BanksCrowdSale.fromAddress(Address.parse(BANK_CROWDSALE_ADDRESS))
+      return client.open(contract) as OpenedContract<BanksCrowdSale>
+    }
+  }, [client])
 
   const bankStakeStorage = useMemo(() => {
     return (address: Address): OpenedContract<StakeStorage> | undefined => {
-      if (!client) return;
-      const contract = StakeStorage.fromAddress(address);
-      return client.open(contract) as OpenedContract<StakeStorage>;
-    };
-  }, [client]);
+      if (!client) return
+      const contract = StakeStorage.fromAddress(address)
+      return client.open(contract) as OpenedContract<StakeStorage>
+    }
+  }, [client])
 
   return {
     jetton: {
       getWallet: async (jettonMasterAddress: Address, ownerAddress: Address) =>
         jettonMaster(jettonMasterAddress)?.getGetWalletAddress(ownerAddress),
 
-      transfer: async (
-        walletAddress: Address,
-        destinationAddress: Address,
-        amount: bigint
-      ) =>
+      transfer: async (walletAddress: Address, destinationAddress: Address, amount: bigint) =>
         jettonWallet(walletAddress)?.send(
           sender,
           {
@@ -98,7 +76,7 @@ function useContracts() {
             // bounce: true,
           },
           {
-            $$type: "JettonTransfer",
+            $$type: 'JettonTransfer',
             query_id: 0n,
             destination: destinationAddress,
             response_destination: destinationAddress,
@@ -106,58 +84,34 @@ function useContracts() {
             forward_ton_amount: 100n,
             custom_payload: beginCell().endCell(),
             forward_payload: beginCell().endCell(),
-          } as JettonTransfer
+          } as JettonTransfer,
         ),
     },
 
     bank: {
       buy: async (amount: bigint) =>
-        bankCrowdSale()?.send(
-          sender,
-          { value: amount + toNano(BANK_GAS_AMOUNT) },
-          "buyBank"
-        ),
+        bankCrowdSale()?.send(sender, { value: amount + toNano(BANK_GAS_AMOUNT) }, 'buyBank'),
       buyWithReferral: async (referralAddress: Address, amount: bigint) =>
-        bankCrowdSale()?.send(
-          sender,
-          { value: amount + toNano(BANK_GAS_AMOUNT) },
-          {
-            $$type: "ReferralAddress",
-            referral: referralAddress,
-          } as ReferralAddress
-        ),
+        bankCrowdSale()?.send(sender, { value: amount + toNano(BANK_GAS_AMOUNT) }, {
+          $$type: 'ReferralAddress',
+          referral: referralAddress,
+        } as ReferralAddress),
       stake: async (walletAddress: Address, amount: bigint) =>
-        bankJettonWallet(walletAddress)?.send(
-          sender,
-          { value: toNano(5 * BANK_GAS_AMOUNT) },
-          {
-            $$type: "Stake",
-            query_id: 0n,
-            amount: amount,
-          } as Stake
-        ),
-      unstake: async () =>
-        bankJettonMaster()?.send(
-          sender,
-          { value: toNano(BANK_GAS_AMOUNT) },
-          "Unstake"
-        ),
-      claim: async () =>
-        bankJettonMaster()?.send(
-          sender,
-          { value: toNano(BANK_GAS_AMOUNT) },
-          "Claim"
-        ),
-      getWallet: async (ownerAddress: Address) =>
-        bankJettonMaster()?.getGetWalletAddress(ownerAddress),
+        bankJettonWallet(walletAddress)?.send(sender, { value: toNano(5 * BANK_GAS_AMOUNT) }, {
+          $$type: 'Stake',
+          query_id: 0n,
+          amount: amount,
+        } as Stake),
+      unstake: async () => bankJettonMaster()?.send(sender, { value: toNano(BANK_GAS_AMOUNT) }, 'Unstake'),
+      claim: async () => bankJettonMaster()?.send(sender, { value: toNano(BANK_GAS_AMOUNT) }, 'Claim'),
+      getWallet: async (ownerAddress: Address) => bankJettonMaster()?.getGetWalletAddress(ownerAddress),
 
-      getStakeAddress: async (ownerAddress: Address) =>
-        bankJettonMaster()?.getCalculateStakeAddress(ownerAddress),
+      getStakeAddress: async (ownerAddress: Address) => bankJettonMaster()?.getCalculateStakeAddress(ownerAddress),
 
       getStakeInfo: async (stakeAddress: Address, ownerAddress: Address) =>
         bankStakeStorage(stakeAddress)?.getAmountTime(ownerAddress),
     },
-  };
+  }
 }
 
-export default useContracts;
+export default useContracts
