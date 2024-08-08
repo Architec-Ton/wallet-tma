f6ce64422c5af62ef20642985cfa39fb33664406
egorkqq <egorkqq@gmail.com>
2024-08-08 21:24:33 +0300

    feat: new endpoint

--- src/utils/pincode.ts
+++ src/utils/pincode.ts
@@ -1,13 +1,13 @@
-import { sha256_sync as sha256sync } from "@ton/crypto";
+import { sha256_sync as sha256Sync } from "@ton/crypto";
 import CryptoES from "crypto-es";
 
 import { TON_PINCODE_SALT } from "../constants";
 
 const encodeMnemonic = (mnemonic: string, salt: string) =>
-  CryptoES.TripleDES.encrypt(mnemonic, sha256sync(salt).toString()).toString();
+  CryptoES.TripleDES.encrypt(mnemonic, sha256Sync(salt).toString()).toString();
 
 const decodeMnemonic = (hash: string, salt: string) =>
-  CryptoES.TripleDES.decrypt(hash, sha256sync(salt).toString()).toString(CryptoES.enc.Utf8);
+  CryptoES.TripleDES.decrypt(hash, sha256Sync(salt).toString()).toString(CryptoES.enc.Utf8);
 
 export const encodePrivateKeyByPin = (mnemonics: string[], pincode: string) =>
   encodeMnemonic(mnemonics.join(" "), `${pincode}-${TON_PINCODE_SALT}`);
