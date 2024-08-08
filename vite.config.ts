8cdf0f075346e927b2b6f442243d8f288f0da0cd
egorkqq <egorkqq@gmail.com>
2024-08-08 19:27:50 +0300

    chore: imports config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import basicSsl from "@vitejs/plugin-basic-ssl";
 import react from "@vitejs/plugin-react-swc";
-import { defineConfig , splitVendorChunkPlugin } from "vite";
+import path from "path";
+import { defineConfig, splitVendorChunkPlugin } from "vite";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 // https://vitejs.dev/config/
@@ -25,7 +26,18 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      "@": "./src",
+      assets: path.resolve(__dirname, "./src/assets"),
+      components: path.resolve(__dirname, "./src/components"),
+      contracts: path.resolve(__dirname, "./src/contracts"),
+      features: path.resolve(__dirname, "./src/features"),
+      hooks: path.resolve(__dirname, "./src/hooks"),
+      i18n: path.resolve(__dirname, "./src/i18n"),
+      mocks: path.resolve(__dirname, "./src/mocks"),
+      pages: path.resolve(__dirname, "./src/pages"),
+      store: path.resolve(__dirname, "./src/store"),
+      types: path.resolve(__dirname, "./src/types"),
+      utils: path.resolve(__dirname, "./src/utils"),
+      constants: path.resolve(__dirname, "./src/constants"),
     },
   },
 });
