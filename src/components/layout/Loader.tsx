3b2c7c624cdb5909ab0a033820b2cc8e7a2b8b0f
egorkqq <egorkqq@gmail.com>
2024-08-26 01:35:59 +0500

    feat: MORE support to Pavel

--- src/components/layout/Loader.tsx
+++ src/components/layout/Loader.tsx
@@ -1,29 +1,55 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
+import type { LottieRefCurrentProps } from "lottie-react";
 import Lottie from "lottie-react";
 
 import loaderAnimationData from "assets/loties/Loading_animation.json";
 
+import Button from "components/buttons/Button";
 import LinkButton from "components/buttons/LinkButton";
 
 import "./Loader.styles.css";
 
-function Loader() {
+function Loader({
+  isLoading,
+  isClicked,
+  onClick,
+}: {
+  isLoading: boolean;
+  isClicked: boolean;
+  onClick: (s: boolean) => void;
+}) {
   const [lottieFileLoaded, setLottieFileLoaded] = useState(false);
+  const lottieRef = useRef<LottieRefCurrentProps>(null);
+
+  useEffect(() => {
+    if (!isClicked && !isLoading && lottieRef.current) {
+      lottieRef.current.pause();
+    }
+  }, [isClicked, isLoading]);
 
   return (
     <>
       <div className="loader">
-        <Lottie onDOMLoaded={() => setLottieFileLoaded(true)} animationData={loaderAnimationData} loop />
-        <p>
-          <LinkButton
-            to="https://chng.it/RyTJYnyZ5z"
-            className="loader-text"
-            style={{ opacity: lottieFileLoaded ? 1 : 0 }}
-          >
+        <Lottie
+          lottieRef={lottieRef}
+          onDOMLoaded={() => setLottieFileLoaded(true)}
+          animationData={loaderAnimationData}
+          loop
+        />
+        <p className="loader-text">
+          <LinkButton to="https://chng.it/RyTJYnyZ5z" style={{ opacity: lottieFileLoaded ? 1 : 0 }}>
             #FreeDurov
           </LinkButton>
         </p>
+
+        <Button
+          style={{ opacity: !isClicked && !isLoading ? 1 : 0 }}
+          onClick={() => onClick(true)}
+          className="loader-button"
+        >
+          Continue
+        </Button>
       </div>
     </>
   );
