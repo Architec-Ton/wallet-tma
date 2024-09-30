2db9f590a938969a240d9110f427dee0bf39021a
Vardan <vardanter@yandex.ru>
2024-09-30 19:19:26 +0400

    add log to wallet menu

--- src/components/ui/menu/WalletMenu.tsx
+++ src/components/ui/menu/WalletMenu.tsx
@@ -16,12 +16,14 @@ import "./WalletMenu.styles.css";
 function WalletMenu() {
   const t = useLanguage("menu-wallet");
 
+  
   const menuItems: MenuItem[] = [
     { to: "/add-crypto", icon: iconMenuWalletAdd, label: t("add") },
     { to: "/send", icon: iconMenuWalletSend, label: t("send") },
     { to: "/", icon: iconMenuWalletSwap, label: t("swap") },
     { to: "/bank", icon: iconMenuWalletBank, label: t("bank") },
   ];
+  console.log("walletMenu", menuItems)
 
   return <Menu menuItems={menuItems} className="wallet-menu block" />;
 }
